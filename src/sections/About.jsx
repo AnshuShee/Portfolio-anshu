bb40841823a8fca4711d777bad6292e07c7a2034
Anshu Shee <anshu.shee.cg@gmail.com>
2026-04-17 14:33:37 +0530

    revert: resume button icon to previous SVG

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -79,19 +79,19 @@ const About = () => {
                                 >
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
-                                        width="20" 
-                                        height="20" 
-                                        viewBox="0 0 48 48"
+                                        width="18" 
+                                        height="18" 
+                                        viewBox="0 0 16 16"
                                         className="transition-transform group-hover:scale-110"
                                     >
                                         <path 
                                             fill="none" 
                                             stroke="currentColor" 
                                             strokeLinecap="round" 
-                                            strokeLinejoin="round" 
-                                            d="M33 42.5h5.5a4 4 0 0 0 4-4v-4.084m-37 2V38.5a4 4 0 0 0 4 4h7.688m0-37H9.5a4 4 0 0 0-4 4v3.172M30.813 5.5H38.5a4 4 0 0 1 4 4v3.172m.778 2.675L22.832 3.541a1.75 1.75 0 0 0-2.389.64l-15.3 26.508a1.75 1.75 0 0 0 .64 2.39h0l20.445 11.806a1.75 1.75 0 0 0 2.39-.64l15.301-26.508a1.75 1.75 0 0 0-.64-2.39m-9.028 3.831l-13.52-7.919m15.275 13.333l-17.233-9.95m15.249 13.403L16.79 18.094" 
-                                            strokeWidth="1.6"
+                                            d="M14.5 4.2V15a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V1A.5.5 0 0 1 2 .5h8.85a.5.5 0 0 1 .36.15l3.15 3.2a.5.5 0 0 1 .14.35zm-10 8.3h7m-7-2h7m-1-10V4a.5.5 0 0 0 .5.5h3.5" 
+                                            strokeWidth="1"
                                         />
+                                        <rect width="8" height="2" x="4" y="7" fill="currentColor" rx=".5" ry=".5"/>
                                     </svg>
                                     View Resume
                                 </a>
